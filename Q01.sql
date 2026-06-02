bf8ade99f7bceb7cf2a85eaa95e6e6a5b7e3553c
Sveloz03 <velozsam@gmail.com>
2026-06-02 10:29:32 -0400

    Q1 & Q2

---
+++ Q01.sql
@@ -0,0 +1,5 @@
+use northwind;
+
+#1. How many suppliers are there?  Use a query! 
+
+SELECT COUNT(*) FROM suppliers
---
+++ Q02.sql
@@ -0,0 +1,5 @@
+use northwind;
+
+#2. What is the sum of all the employee's salaries? 
+
+SELECT SUM(Salary) FROM employees
